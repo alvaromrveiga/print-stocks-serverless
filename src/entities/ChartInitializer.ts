466bf9d905c38a9b522ab78e3d10cf64ffb618e7
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-25 13:56:55 -0300

    Add double exponential averages

--- src/entities/ChartInitializer.ts
+++ src/entities/ChartInitializer.ts
@@ -10,6 +10,8 @@ export class ChartInitializer {
     await this.changeToLogView();
 
     await this.closeSideTab();
+
+    await this.addDoubleExponentialMovingAverages();
   }
 
   private async waitPageLoad(): Promise<void> {
@@ -46,6 +48,127 @@ export class ChartInitializer {
       await barHider?.click();
     } catch (error) {
       throw new Error("Error closing the side tab: " + error);
+    }
+  }
+
+  private async addDoubleExponentialMovingAverages(): Promise<void> {
+    await this.openIndicatorsTab();
+
+    await this.inputIndicator("EMA");
+
+    await this.clickBestIndicatorMatch();
+    await this.clickBestIndicatorMatch();
+
+    await this.closeIndicatorsTab();
+
+    await this.openIndicatorConfig(2);
+
+    await this.changePeriodValue("72");
+
+    await this.confirmIndicatorChanges();
+  }
+
+  private async openIndicatorsTab(): Promise<void> {
+    try {
+      const indicatorsTab = await this.page.waitForXPath(
+        '//div[@id="header-toolbar-indicators"]',
+        { timeout: DEFAULT_TIMEOUT }
+      );
+
+      await indicatorsTab?.click();
+    } catch (error) {
+      throw new Error(`Error opening the indicators tab: ` + error);
+    }
+  }
+
+  private async inputIndicator(indicator: string): Promise<void> {
+    try {
+      const indicatorInput = await this.page.waitForXPath(
+        '//input[@class="input-CcsqUMct"]',
+        { timeout: DEFAULT_TIMEOUT }
+      );
+
+      await indicatorInput?.focus();
+
+      await this.page.keyboard.type(indicator);
+    } catch (error) {
+      throw new Error(`Error inputting exponential moving average: ` + error);
+    }
+  }
+
+  private async clickBestIndicatorMatch(): Promise<void> {
+    try {
+      await this.page.waitForXPath('(//div[@class="main-FkkXGK5n"])[1]', {
+        timeout: DEFAULT_TIMEOUT,
+      });
+
+      await this.page.$eval("div.main-FkkXGK5n", (elem) =>
+        (elem as HTMLElement).click()
+      );
+    } catch (error) {
+      throw new Error(`Error clicking best indicator match: ` + error);
+    }
+  }
+
+  private async closeIndicatorsTab(): Promise<void> {
+    try {
+      const closeButton = await this.page.waitForXPath(
+        '//span[@class="close-tuOy5zvD"]',
+        {
+          timeout: DEFAULT_TIMEOUT,
+        }
+      );
+
+      await closeButton?.click();
+    } catch (error) {
+      throw new Error(`Error closing indicators tab: ` + error);
+    }
+  }
+
+  private async openIndicatorConfig(index: number = 1): Promise<void> {
+    try {
+      const indicator = await this.page.waitForXPath(
+        `(//div[@class="sourcesWrapper-OYqjX7Sg"]//div[@class="titleWrapper-OYqjX7Sg"])[${index}]`,
+        {
+          timeout: DEFAULT_TIMEOUT,
+        }
+      );
+
+      await indicator?.click();
+      await indicator?.click();
+    } catch (error) {
+      throw new Error(`Error opening indicator ${index} config: ` + error);
+    }
+  }
+
+  private async changePeriodValue(value: string): Promise<void> {
+    try {
+      const periodInput = await this.page.waitForXPath(
+        '(//input[starts-with(@class, "input-uGWFLwEy")])[1]',
+        {
+          timeout: DEFAULT_TIMEOUT,
+        }
+      );
+
+      await periodInput?.focus();
+      await this.page.keyboard.type(value);
+    } catch (error) {
+      throw new Error(`Error changing period value: ` + error);
+    }
+  }
+
+  private async confirmIndicatorChanges(): Promise<void> {
+    try {
+      const confirmButton = await this.page.waitForXPath(
+        '//button[@name="submit" and starts-with(@class, "button-YKkCvwjV")]',
+        {
+          timeout: DEFAULT_TIMEOUT,
+        }
+      );
+
+      await confirmButton?.click();
+    } catch (error) {
+      throw new Error(`Error confirming indicator changes: ` + error);
     }
   }
 }
